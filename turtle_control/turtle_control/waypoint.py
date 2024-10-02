4755da26937639d409e7d24a0b1eec31654d3240
Logan Boswell <loganstuartboswell@gmail.com>
2024-10-02 18:05:55 -0500

    Created basic toggle service in waypoint node

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -2,6 +2,7 @@
 
 import rclpy
 from rclpy.node import Node
+from std_srvs.srv import Empty
 
 class Waypoint(Node):
 
@@ -11,9 +12,27 @@ class Waypoint(Node):
         self.declare_parameter('frequency', 90)
         self._freq = self.get_parameter('frequency').value
         self._tmr = self.create_timer(1/self._freq, self.timer_callback)
+        self._toggle = self.create_service(Empty, 'toggle', self.toggle_callback)
+        self._state = 'STOPPED'
+        self._first_log_for_stop = True
 
     def timer_callback(self):
-        self.get_logger().debug('Issuing Command!')
+        if self._state == 'STOPPED':
+            if self._first_log_for_stop:
+                self.get_logger().info('Stopping.')
+                self._first_log_for_stop = False
+        elif self._state == 'MOVING':
+            self.get_logger().error('Issuing Command!')
+        
+    def toggle_callback(self, request, response):
+        if self._state == 'STOPPED':
+            self._state = 'MOVING'
+        elif self._state == 'MOVING':
+            self._state = 'STOPPED'
+            self._first_log_for_stop = True
+        return response
+
+        
 
 def main(args=None):
     """Entrypoint for the mynode ROS node."""
