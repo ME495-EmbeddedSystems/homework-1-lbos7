fb7dab205d7266d737a62081182007ecea6fe896
Logan Boswell <loganstuartboswell@gmail.com>
2024-10-02 22:00:16 -0500

    Added mover.py to repo; Added trail marker for toggle service (needs testing)

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -3,6 +3,15 @@
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Empty
+from turtlesim.srv import SetPen
+from enum import Enum, auto
+
+class State(Enum):
+    """ Current state of the system.
+        Determines what the main timer function should be doing on each iteration
+    """
+    STOPPED = auto(),
+    MOVING = auto()
 
 class Waypoint(Node):
 
@@ -10,26 +19,31 @@ class Waypoint(Node):
         super().__init__('waypoint')
         self.get_logger().info('Waypoint')
         self.declare_parameter('frequency', 90)
-        self._freq = self.get_parameter('frequency').value
-        self._tmr = self.create_timer(1/self._freq, self.timer_callback)
-        self._toggle = self.create_service(Empty, 'toggle', self.toggle_callback)
-        self._state = 'STOPPED'
-        self._first_log_for_stop = True
+        self.freq = self.get_parameter('frequency').value
+        self.tmr = self.create_timer(1/self.freq, self.timer_callback)
+        self.toggle = self.create_service(Empty, 'toggle', self.toggle_callback)
+        self.path_client = self.create_client(SetPen, 'set_pen')
+        self.state = State.STOPPED
+        self.first_log_for_stop = True
 
     def timer_callback(self):
-        if self._state == 'STOPPED':
-            if self._first_log_for_stop:
+        if self.state == State.STOPPED:
+            if self.first_log_for_stop:
                 self.get_logger().info('Stopping.')
-                self._first_log_for_stop = False
-        elif self._state == 'MOVING':
+                self.first_log_for_stop = False
+        elif self.state == State.MOVING:
             self.get_logger().error('Issuing Command!')
         
     def toggle_callback(self, request, response):
-        if self._state == 'STOPPED':
-            self._state = 'MOVING'
-        elif self._state == 'MOVING':
-            self._state = 'STOPPED'
-            self._first_log_for_stop = True
+        if self.state == State.STOPPED:
+            self.state = State.MOVING
+            self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
+                                                       width=3, off=0))
+        elif self.state == State.MOVING:
+            self.state = State.STOPPED
+            self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
+                                                       width=3, off=1))
+            self.first_log_for_stop = True
         return response
 
         
