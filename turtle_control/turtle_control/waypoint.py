1786cd57888f289544bc387d57d781fcdcb01aa1
Logan Boswell <loganstuartboswell@gmail.com>
2024-10-05 23:34:07 -0500

    Added load waypoints service (needs work) and created subscriber for turtle1/pose

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -3,8 +3,12 @@
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import Empty
-from turtlesim.srv import SetPen
+from turtlesim.srv import SetPen, TeleportAbsolute, TeleportRelative
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist, Vector3
+from turtle_interfaces.srv import Waypoints
 from enum import Enum, auto
+import math
 
 class State(Enum):
     """ Current state of the system.
@@ -22,9 +26,16 @@ class Waypoint(Node):
         self.freq = self.get_parameter('frequency').value
         self.tmr = self.create_timer(1/self.freq, self.timer_callback)
         self.toggle = self.create_service(Empty, 'toggle', self.toggle_callback)
+        self.load = self.create_service(Waypoints, 'load', self.load_callback)
         self.path_client = self.create_client(SetPen, 'set_pen')
+        self.reset_client = self.create_client(Empty, 'reset')
+        self.teleport_abs_client = self.create_client(TeleportAbsolute, 'teleport_absolute')
+        self.teleport_rel_client = self.create_client(TeleportRelative, 'teleport_relative')
+        self.pose = self.create_subscription(Pose, 'pose', self.pose_callback, 10)
         self.state = State.STOPPED
         self.first_log_for_stop = True
+        self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
+                                                       width=3, off=1))
 
     def timer_callback(self):
         if self.state == State.STOPPED:
@@ -32,7 +43,7 @@ class Waypoint(Node):
                 self.get_logger().info('Stopping.')
                 self.first_log_for_stop = False
         elif self.state == State.MOVING:
-            self.get_logger().error('Issuing Command!')
+            self.get_logger().debug('Issuing Command!')
         
     def toggle_callback(self, request, response):
         if self.state == State.STOPPED:
@@ -45,11 +56,104 @@ class Waypoint(Node):
                                                        width=3, off=1))
             self.first_log_for_stop = True
         return response
+    
+    def load_callback(self, request, response):
+        is_first_point = True
+        first_point = {}
+        prev_point = {}
+        if len(request.waypoints) > 0:
+            for i in range(len(request.waypoints)):
+                if not is_first_point:
+                    response.distance += math.dist([request.waypoints[i].x,
+                                           request.waypoints[i].y],
+                                          [prev_point.x, prev_point.y])
+                    prev_point = request.waypoints[i]
+                else:
+                    prev_point = request.waypoints[i]
+                    first_point = request.waypoints[i]
+                    is_first_point = False
+            self.draw_x(request.waypoints)
+            self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+                x=first_point.x, y=first_point.y, theta=0.0))
+        self.state = State.STOPPED
+        return response
+
+    # def load_callback(self, request, response):
+    #     self.reset_client.call_async(Empty.Request(name='turtle1'))
+    #     is_first_point = True
+    #     first_point = {}
+    #     prev_point = {}
+    #     self.path_client.call_async(SetPen.Request(r=255, g=0, b=0,
+    #                                                    width=3, off=0))
+    #     if len(request.waypoints) > 0:
+    #         for i in range(len(request.waypoints)):
+    #             if not is_first_point:
+    #                 response.distance += math.dist([request.waypoints[i].x,
+    #                                        request.waypoints[i].y],
+    #                                       [prev_point.x, prev_point.y])
+    #                 self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+    #                     x=request.waypoints[i].x, y=request.waypoints[i].y, theta=0))
+    #                 prev_point = request.waypoints[i]
+    #             else:
+    #                 self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+    #                     x=request.waypoints[i].x, y=request.waypoints[i].y, theta=0))
+    #                 prev_point = request.waypoints[i]
+    #                 first_point = request.waypoints[i]
+    #                 is_first_point = False
+    #         self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+    #             x=first_point.x, y=first_point.y, theta=0.0))
+    #     self.state = State.STOPPED
+    #     self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
+    #                                                    width=3, off=1))
+    #     return response
+        
+        
+    
+    async def draw_x(self, points):
+        for point in points:
+            await self.path_client.call_async(SetPen.Request(r=255, g=0, b=0,
+                                                        width=3, off=1))
+            self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+                x=point.x, y=point.y, theta=0.0))
+            await self.path_client.call_async(SetPen.Request(r=255, g=0, b=0,
+                                                        width=3, off=0))
+            self.teleport_rel_client.call_async(TeleportRelative.Request(
+                linear=.2, angular=math.pi/4))
+            self.teleport_rel_client.call_async(TeleportRelative.Request(
+                linear=.4, angular=math.pi))
+            self.teleport_rel_client.call_async(TeleportRelative.Request(
+                linear=.2, angular=math.pi))
+            self.teleport_rel_client.call_async(TeleportRelative.Request(
+                linear=.2, angular=math.pi/2))
+            self.teleport_rel_client.call_async(TeleportRelative.Request(
+                linear=.4, angular=math.pi))
+
+    # def draw_x(self, point):
+    #     self.path_client.call_async(SetPen.Request(r=255, g=0, b=0,
+    #                                                    width=3, off=0))
+    #     self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+    #         x=point.x, y=point.y, theta=0.0))
+    #     self.get_logger().error('center of point')
+    #     self.teleport_rel_client.call_async(TeleportRelative.Request(
+    #         linear=.2, angular=math.pi/4))
+    #     self.teleport_rel_client.call_async(TeleportRelative.Request(
+    #         linear=.4, angular=math.pi))
+    #     self.teleport_abs_client.call_async(TeleportAbsolute.Request(
+    #         x=point.x, y=point.y, theta=math.pi))
+    #     self.teleport_rel_client.call_async(TeleportRelative.Request(
+    #         linear=.2, angular=-math.pi/4))
+    #     self.teleport_rel_client.call_async(TeleportRelative.Request(
+    #         linear=.4, angular=math.pi))
+    #     self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
+    #                                                    width=3, off=1))
+
+
+    # def pose_callback(self, pose):
 
         
 
 def main(args=None):
-    """Entrypoint for the mynode ROS node."""
+    """Entrypoint for the waypoint ROS node."""
     rclpy.init(args=args)
     node = Waypoint()
     rclpy.spin(node)
