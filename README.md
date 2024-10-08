8a1f679c3d64b0a032e187ec4f0022376e952c89
Logan Boswell <loganstuartboswell@gmail.com>
2024-10-07 23:53:47 -0500

    Added comments to waypoint.py and explanation to README.md of running turtle.bag in new turtlesim window

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Author: Logan Boswell
 1. Use `ros2 launch turtle_control waypoints.launch.xml` to run the code
 2. The `ros2 service call /load turtle_interfaces/srv/Waypoints "{waypoints: [{x: 1.4, y: 1.6}, {x: 2.2, y: 9.4}, {x: 7.2, y: 6.1}, {x: 4.0, y: 2.6}, {x: 8.2, y: 1.5}, {x: 4.1, y: 5.3}]}"` service loads waypoints for the turtle to follow
 3. The `ros2 service call /toggle std_srvs/srv/Empty` starts and stops the turtle.
-4. Here is a video of the turtle in action.
+4. Here is a video of the turtle in action with the commands above.
     <video src="https://github-production-user-asset-6210df.s3.amazonaws.com/181179449/374396453-6bfe70e7-9a13-4a59-851a-26d4da715280.webm?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20241008%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241008T033130Z&X-Amz-Expires=300&X-Amz-Signature=bbe5dfd01f19661fd6ccc5b7841088719be2ba4a3f81cf24611102467e812d2e&X-Amz-SignedHeaders=host" width="500" />
+5. When using the turtle.bag data in a new turtlesim window, the turtle tries to complete the same movements as in the video, but runs into the walls of the window since the turtle is not starting from the same location as the first waypoint in the video
--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -1,5 +1,4 @@
-
-
+""" Class for a waypoint node in the turtle_control package """
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
@@ -20,25 +19,62 @@ class State(Enum):
     MOVING = auto()
 
 class Waypoint(Node):
+    """
+    Node for controlling a turtle's movement towards waypoints in turtlesim.
+
+    Publishes
+    ---------
+    cmd_vel : geometry_msgs/msg/Twist - velocity to control the turtle
+    loop_metrics : turtle_interfaces/msg/ErrorMetric - information about turtle's motion
+
+    Subscribes
+    ----------
+    pose : turtlesim/msg/Pose - the pose of the turtle
+
+    Services
+    ----------
+    toggle : std_srvs/srv/Empty - changes turtle's current state
+    load : turtle_interfaces/srv/Waypoints - loads a set of waypoints for the turtle to move towards
+
+    Service Clients
+    ----------
+    set_pen : turtlesim/srv/SetPen - changes pen settings for turtle
+    reset : std_srvs/srv/Empty - loads a set of waypoints for the turtle to move towards
+    teleport_absolute : turtlesim/srv/TeleportAbsolute - teleports the turtle to a given x, y, and theta position
+    teleport_relative : turtlesim/srv/TeleportRelative - teleports the a linear distance and angular distance away from current position
+
+    Parameters
+    ----------
+    frequency : float64 - the frequency at which the timer callback is executed
+    tolerance : float64 - the tolerance for if a turtle has reached a waypoint
+    """
 
     def __init__(self):
         super().__init__('waypoint')
         cb_group = MutuallyExclusiveCallbackGroup()
+
         self.get_logger().info('Waypoint')
+
         self.declare_parameter('frequency', 90.0)
         self.declare_parameter('tolerance', .05)
         self.freq = self.get_parameter('frequency').value
         self.tolerance = self.get_parameter('tolerance').value
+
         self.tmr = self.create_timer(1/self.freq, self.timer_callback)
+
         self.toggle = self.create_service(Empty, 'toggle', self.toggle_callback)
         self.load = self.create_service(Waypoints, 'load', self.load_callback)
+
         self.path_client = self.create_client(SetPen, 'set_pen', callback_group=cb_group)
         self.reset_client = self.create_client(Empty, 'reset', callback_group=cb_group)
         self.teleport_abs_client = self.create_client(TeleportAbsolute, 'teleport_absolute', callback_group=cb_group)
         self.teleport_rel_client = self.create_client(TeleportRelative, 'teleport_relative', callback_group=cb_group)
+        
         self.pose_sub = self.create_subscription(Pose, 'pose', self.pose_callback, 10)
+        
         self.vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
         self.error_pub = self.create_publisher(ErrorMetric, 'loop_metrics', 10)
+        
         self.state = State.STOPPED
         self.first_log_for_stop = True
         self.pose = []
@@ -58,23 +94,42 @@ class Waypoint(Node):
 
 
     def timer_callback(self):
+        """ Callback for timer
+        
+        Publishes commands at a set frequency paramter (commands vary based on turtle state)
+        """
         if self.state == State.STOPPED:
             if self.first_log_for_stop:
                 self.get_logger().info('Stopping.')
                 self.first_log_for_stop = False
             self.vel_pub.publish(Twist(linear=Vector3(x=0.0), angular=Vector3(z=0.0)))
+
         elif self.state == State.MOVING:
             self.get_logger().debug('Issuing Command!')
+
+            # Only moves the turtle if there are waypoints for the turtle to follow
             if len(self.waypoints) > 0:
+
+                # Finding Errors used for P-Control
                 target = self.waypoints[self.current_waypoint_ind]
                 self.distance_error = math.dist([target.x, target.y], [self.pose.x, self.pose.y])
                 self.angle_error = math.atan2(target.y - self.pose.y, target.x - self.pose.x) - self.pose.theta
+
+                # Adjusting for if the magnitude of the error is greater than pi
                 if self.angle_error > math.pi:
                     self.angle_error = self.angle_error - 2*math.pi
                 elif self.angle_error < -math.pi:
                     self.angle_error = 2*math.pi + self.angle_error
-                self.vel_pub.publish(Twist(linear=Vector3(x=max(self.lin_gain*self.distance_error, self.minimum_vel)), angular=Vector3(z=self.angle_gain*self.angle_error)))
+
+                # Publishing to cmd_vel (linear speed has a minimum value to avoid slowing when approaching a waypoint)
+                self.vel_pub.publish(
+                    Twist(linear=Vector3(x=max(self.lin_gain*self.distance_error, self.minimum_vel)),
+                          angular=Vector3(z=self.angle_gain*self.angle_error)))
+                
+                # Checking to see if the turtle is close enough to waypoint
                 if self.distance_error < self.tolerance:
+
+                    # Setting next waypoint for turtle to move towards and publishing to /loop_metrics if applicable
                     if self.current_waypoint_ind == (len(self.waypoints) - 1):
                         self.current_waypoint_ind -= 1
                         self.waypoints_forward = False
@@ -91,6 +146,16 @@ class Waypoint(Node):
 
                 
     def toggle_callback(self, request, response):
+        """ Callback for the toggle service
+
+        Changes the turtle's current state & adjusts pen
+
+            Args:
+                request (Empty)
+
+            Returns:
+                response (Empty)
+        """
         if self.state == State.STOPPED:
             self.state = State.MOVING
             self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
@@ -104,17 +169,38 @@ class Waypoint(Node):
 
 
     async def load_callback(self, request, response):
+        """ Callback function for the load service
+
+        Resets turtlesim window and variables related to traversing waypoints,
+        loads waypoints, and draws an X at each waypoint
+
+            Args:
+                request (Waypoints_Request) : a list of point dictionaries
+
+            Returns:
+                response (Waypoints_Response) : the straight-line distance
+                between all of the waypoints
+        """
+
+        # Resetting variables and turtlesim window for new waypoints
         self.waypoints = request.waypoints
         self.current_waypoint_ind = 1
+        self.waypoints_forward = True
         self.complete_loops = 0
         self.actual_distance = 0.0
         self.error = 0.0
         self.error_pub.publish(ErrorMetric(complete_loops=self.complete_loops, actual_distance=self.actual_distance, error=self.error))
         await self.reset_client.call_async(Empty.Request())
+
+        
         is_first_point = True
         first_point = {}
         prev_point = {}
+
+        # Only runs if waypoints are given
         if len(request.waypoints) > 0:
+
+            # Calculating straight-line distance between waypoints
             for i in range(len(request.waypoints)):
                 if not is_first_point:
                     response.distance += math.dist([request.waypoints[i].x,
@@ -125,19 +211,31 @@ class Waypoint(Node):
                     prev_point = request.waypoints[i]
                     first_point = request.waypoints[i]
                     is_first_point = False
+
+            # Drawing markers and then moving turtle to first waypoint
             await self.draw_x(request.waypoints)
             self.path_client.call_async(SetPen.Request(r=255, g=255, b=255,
                                                         width=3, off=1))
             await self.teleport_abs_client.call_async(TeleportAbsolute.Request(
                 x=first_point.x, y=first_point.y, theta=0.0))
+            
+        # Stopping turtle if it was moving when the load service was called
         if self.state == State.MOVING:
             self.state = State.STOPPED
             self.first_log_for_stop = True
+
+        # Saving straight-line distance and returning response
         self.sl_dist = response.distance
         return response
         
     
     async def draw_x(self, points):
+        """ Draws an X at each of the given points
+
+            Args:
+                points (list of point dictionaries) : a list of point dictionaries
+                where the markers should be drawn
+        """
         for point in points:
             self.path_client.call_async(SetPen.Request(r=0, g=0, b=0,
                                                         width=3, off=1))
@@ -155,10 +253,18 @@ class Waypoint(Node):
                 linear=.2, angular=math.pi/2))
             await self.teleport_rel_client.call_async(TeleportRelative.Request(
                 linear=.4, angular=math.pi))
+    
             
     def pose_callback(self, pose):
+        """ Updates the self.pose, self.prev_pose, and self.actual_distance variables 
+        
+            Args:
+                pose (dictionary) : the current pose of the turtle
+        """
         self.prev_pose = self.pose
         self.pose = pose
+
+        # if the current and previous poses are both the same type, update the actual distance travelled by the turtle
         if type(self.pose) == type(self.prev_pose):
             self.actual_distance += math.dist([self.pose.x, self.pose.y],
                                             [self.prev_pose.x, self.prev_pose.y])
